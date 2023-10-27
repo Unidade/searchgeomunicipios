48d583bdc21b6b666af0a8d6a206d3682095c5bd
Dario Rodrigues <dario.rodri123@gmail.com>
2023-10-27 17:18:10 -0300

    chore update types and node image

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,9 +1,7 @@
-import * as React from "react";
-
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export const Button = ({ children, ...rest }: ButtonProps) => {
-  return <button {...rest}>{children}</button>;
-};
+  return <button {...rest}>{children}</button>
+}
